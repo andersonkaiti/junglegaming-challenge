b7ccb320d938fd2ec0432e0c8d07360758d3cacd
andersonkaiti <anderkaiti@gmail.com>
2025-10-16 13:38:06 -0300

    :sparkles: feat(auth-service,users): add timestamps and constraints to users table with migration and update user entity

--- apps/auth-service/src/database/typeorm.datasource.ts
+++ apps/auth-service/src/database/typeorm.datasource.ts
@@ -4,6 +4,7 @@ import { DataSource } from 'typeorm'
 import { Env } from '../env.schema'
 import User from './entities/user.entity'
 import { CreateUsersTable1760548535462 } from './migrations/1760548535462-create-users-table'
+import { AddTimestampsAndConstraintsToUsersTable1760628230852 } from './migrations/1760628230852-add-timestamps-and-constraints-to-users-table'
 
 config()
 
@@ -17,7 +18,10 @@ const datasource = new DataSource({
   password: configService.get('POSTGRES_PASSWORD', { infer: true }),
   database: configService.get('POSTGRES_DB', { infer: true }),
   entities: [User],
-  migrations: [CreateUsersTable1760548535462],
+  migrations: [
+    CreateUsersTable1760548535462,
+    AddTimestampsAndConstraintsToUsersTable1760628230852,
+  ],
   synchronize: false,
 })
 
