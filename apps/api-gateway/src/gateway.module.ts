4a1eb25d954b38246060d755599b98b2a849f28c
andersonkaiti <anderkaiti@gmail.com>
2025-10-17 23:25:01 -0300

    :sparkles: feat(api-gateway,throttler): integrate throttling functionality with @nestjs/throttler and configure global throttler settings in the gateway module

--- apps/api-gateway/src/gateway.module.ts
+++ apps/api-gateway/src/gateway.module.ts
@@ -2,16 +2,28 @@ import { RabbitMQModule } from '@junglegaming-challenge/rabbitmq'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { RouterModule } from '@nestjs/core'
+import { ThrottlerModule } from '@nestjs/throttler'
 import { envSchema } from './env.schema'
 import { AuthModule } from './jwt/auth.module'
 import { AuthGatewayModule } from './microservices/auth/gateway.module'
 import { TasksGatewayModule } from './microservices/tasks/gateway.module'
+
+const ONE_SECOND = 1000
+const TEN_SECONDS = 10 * ONE_SECOND
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: (env) => envSchema.parse(env),
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: TEN_SECONDS,
+          limit: 10,
+        },
+      ],
     }),
     RabbitMQModule,
     RouterModule.register([
