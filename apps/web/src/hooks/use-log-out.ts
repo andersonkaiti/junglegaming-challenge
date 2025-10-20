940cff766a67bcbe125d139fb460221868f93aff
andersonkaiti <anderkaiti@gmail.com>
2025-10-20 19:26:43 -0300

    :bug: fix(web,log-out): resolve logout issue by redirecting to sign-in page and success toast

--- apps/web/src/hooks/use-log-out.ts
+++ apps/web/src/hooks/use-log-out.ts
@@ -1,8 +1,19 @@
+import { useNavigate } from '@tanstack/react-router'
 import { removeToken } from '@utils/remove-token'
+import { toast } from 'sonner'
 
 export function useLogOut() {
+  const navigate = useNavigate()
+
   function logOut() {
     removeToken()
+
+    toast.success('Deslogado com sucesso!')
+
+    navigate({
+      to: '/auth/sign-in',
+      replace: true,
+    })
   }
 
   return {
