7e88a591171e1058512b675fe0708e6a5f599c43
andersonkaiti <anderkaiti@gmail.com>
2025-10-20 21:26:05 -0300

    :bug: fix(web,log-out): remove use of removeToken and update logout behavior

--- apps/web/src/hooks/use-log-out.ts
+++ apps/web/src/hooks/use-log-out.ts
@@ -1,12 +1,11 @@
 import { useNavigate } from '@tanstack/react-router'
-import { removeToken } from '@utils/remove-token'
 import { toast } from 'sonner'
 
 export function useLogOut() {
   const navigate = useNavigate()
 
   function logOut() {
-    removeToken()
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/'
 
     toast.success('Deslogado com sucesso!')
 
